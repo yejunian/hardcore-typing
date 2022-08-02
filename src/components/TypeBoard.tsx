a0e54607ea60f87385e988abd8f377884b918835
yejunian <leeye51456@gmail.com>
2022-08-03 01:39:57 +0900

    Feat(TypeBoard): Implement lock time after failure

--- src/components/TypeBoard.tsx
+++ src/components/TypeBoard.tsx
@@ -3,6 +3,7 @@ import React, { useMemo, useState } from 'react'
 type TypeBoardProps = {
   sentence: string
   enabled?: boolean
+  lockTimeAfterFail?: number
   onSucceed?: (userText: string) => any
   onFail?: (userText: string) => any
   onReset?: (userText: string) => any
@@ -30,15 +31,20 @@ const resetKeys = new Set([
 function TypeBoard({
   sentence,
   enabled = true,
+  lockTimeAfterFail = 500,
   onSucceed = () => {},
   onFail = () => {},
   onReset = () => {},
 }: TypeBoardProps) {
+  const [locked, setLocked] = useState(false)
   const [userText, setUserText] = useState('')
+
   const words = useMemo(() => sentence.split(' '), [sentence])
 
+  const typable = enabled && !locked
+
   const handleUserTextInput = (event: React.FormEvent<HTMLInputElement>) => {
-    if (!enabled) {
+    if (!typable) {
       return
     }
 
@@ -53,10 +59,16 @@ function TypeBoard({
       return
     }
 
-    if (
-      currentWords[lastComparableWordIndex] !== words[lastComparableWordIndex]
-    ) {
+    const enteredWord = currentWords[lastComparableWordIndex]
+    const expectedWord = words[lastComparableWordIndex]
+
+    if (enteredWord !== expectedWord) {
+      setLocked(true)
       onFail(userText)
+      window.setTimeout(() => {
+        setUserText('')
+        setLocked(false)
+      }, lockTimeAfterFail)
     } else if (currentUserText === sentence + ' ') {
       setUserText('')
       onSucceed(userText)
@@ -66,6 +78,7 @@ function TypeBoard({
   const handleUserTextKeyUp = (event: React.KeyboardEvent) => {
     if (resetKeys.has(event.code)) {
       setUserText('')
+      setLocked(false)
       onReset(userText)
     }
   }
