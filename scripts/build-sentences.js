599642d3265efcc76dc111f09b41d6088f1acde4
yejunian <leeye51456@gmail.com>
2022-08-07 03:10:46 +0900

    Build(sentences): Implement build-sentences

---
+++ scripts/build-sentences.js
@@ -0,0 +1,63 @@
+const fs = require('fs/promises')
+const path = require('path')
+
+/**
+ * @param {string} dir
+ * @param {string} refFile
+ */
+async function buildSentences(dir, refFile) {
+  const targetDirectory = path.resolve(dir)
+
+  const ref = await getReferences(dir, refFile)
+
+  const srcFiles = await fs.readdir(targetDirectory)
+  const data = []
+
+  for (const srcFile of srcFiles) {
+    if (srcFile === refFile) {
+      continue
+    }
+
+    const contents = await fs.readFile(
+      `${targetDirectory}/${srcFile}`,
+      { encoding: 'utf-8' }
+    )
+
+    for (const line of contents.split('\n')) {
+      const sentence = line.trim()
+      if (!sentence) {
+        continue
+      }
+
+      data.push({
+        sentence,
+        ref: ref.get(srcFile),
+      })
+    }
+  }
+
+  await fs.writeFile(
+    `${targetDirectory}/../sentences.json`,
+    JSON.stringify(data)
+  )
+  console.log(`[build-sentences] ${data.length} sentences have been generated.`)
+}
+
+/**
+ * @param {string} targetDirectory
+ * @param {string} refFile
+ * @returns {Promise<Map<string, string>>}
+ */
+async function getReferences(targetDirectory, refFile) {
+  /** @type {{ filename: string, ref: string }[]} */
+  const parsed = JSON.parse(await fs.readFile(`${targetDirectory}/${refFile}`))
+
+  const map = new Map()
+  for (const entry of parsed) {
+    map.set(entry.filename, entry.ref)
+  }
+
+  return map
+}
+
+buildSentences('src/data/sentences', 'references.json')
