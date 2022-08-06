322812294f18a59b30ae4ed08ce33ee4a14381f1
yejunian <leeye51456@gmail.com>
2022-08-07 03:15:16 +0900

    Feat(sentences): Define type of sentences object

--- src/data/sentences.ts
+++ src/data/sentences.ts
@@ -1,10 +1,10 @@
-const sentences = [
-  '같은 값이면 다홍치마',
-  '구렁이 담 넘어가듯',
-  '귀에 걸면 귀걸이 코에 걸면 코걸이',
-  '닭 쫓던 개 지붕 쳐다보듯',
-  '밑 빠진 독에 물 붓기',
-  '아니 땐 굴뚝에 연기 날까',
-]
+import sentences from './sentences.json'
 
-export default sentences
+type SentenceData = SentenceEntry[]
+
+type SentenceEntry = {
+  sentence: string
+  ref: string
+}
+
+export default sentences as SentenceData
